f3c3e41374c01d514b8fcbd126b43bbcc1735053
Madusanka Palihakoon <pmsmpalihakoon@gmail.com>
2023-10-27 08:34:05 +0530

    add profile_setup for ajax

--- controller/profileSetupAction.php
+++ controller/profileSetupAction.php
@@ -1,9 +1,6 @@
 <?php
-// Check if it's a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Access the values from the FormData object
-    $profile_picture = $_FILES['profile_picture']; // For file upload
-    $cover_picture = $_FILES['cover_picture']; // For file upload
+    // Handle form data here
     $home_town = $_POST['home_town'];
     $contact_info = $_POST['contact_info'];
     $education = $_POST['education'];
@@ -11,19 +8,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $relationship_status = $_POST['relationship_status'];
     $hobbies = $_POST['hobbies'];
 
-    // Now, you can print or process these values as needed
-    echo "Profile Picture:\n";
-    print_r($profile_picture);
+    $profilePicDir = __DIR__."/../assets/uploads/profilePic"; // The folder where you want to store the images
+    $coverPicDir = __DIR__."/../assets/uploads/coverPic"; // The folder where you want to store the images
 
-    echo "Cover Picture:\n";
-    print_r($cover_picture);
+    $profile_picture = $_FILES['profile_picture']['tmp_name'];
+    $cover_picture = $_FILES['cover_picture']['tmp_name'];
+    $profilePic = $profilePicDir . '/' . basename($_FILES['profile_picture']['name']);
+    $coverPic = $coverPicDir . '/' . basename($_FILES['cover_picture']['name']);
 
-    echo "Home Town: " . $home_town . "\n";
-    echo "Contact Info: " . $contact_info . "\n";
-    echo "Education: " . $education . "\n";
-    echo "Employment: " . $employment . "\n";
-    echo "Relationship Status: " . $relationship_status . "\n";
-    echo "Hobbies: " . $hobbies . "\n";
-} else {
-    echo 'no';
+    // Validate file type and size
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    $maxFileSize = 5 * 1024 * 1024; // 5MB
+
+    $profilePicExtension = strtolower(pathinfo($profilePic, PATHINFO_EXTENSION));
+    $coverPicExtension = strtolower(pathinfo($coverPic, PATHINFO_EXTENSION));
+
+    if (!in_array($profilePicExtension, $allowedExtensions) || !in_array($coverPicExtension, $allowedExtensions)) {
+        echo 'Invalid file type. Please upload an image (jpg, jpeg, png, gif).';
+    } elseif ($_FILES['profile_picture']['size'] > $maxFileSize || $_FILES['cover_picture']['size'] > $maxFileSize) {
+        echo 'File size exceeds the limit (5MB).';
+    } elseif (move_uploaded_file($profile_picture, $profilePic) && move_uploaded_file($cover_picture, $coverPic)) {
+        // Files uploaded successfully
+        // Connect to the database and insert form data
+        $db = new mysqli("localhost", "root", "", "vicenet");
+
+        if ($db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }
+
+        $sql = "INSERT INTO user_data (home_town, contact_info, education, employment, relationship_status, hobbies, profile_pic_path, cover_pic_path) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = $db->prepare($sql);
+        $stmt->bind_param("ssssssss", $home_town, $contact_info, $education, $employment, $relationship_status, $hobbies, $profilePic, $coverPic);
+
+        if ($stmt->execute()) {
+            echo 'Data saved successfully';
+        } else {
+            echo 'Failed to save data to the database';
+        }
+
+        $stmt->close();
+        $db->close();
+    } else {
+        echo 'Failed to move uploaded files';
+    }
 }
--- pages/profile_setup.php
+++ pages/profile_setup.php
@@ -101,6 +101,37 @@ require_once __DIR__."/../config/SessionConfig.php";
     }
     /* profile_setup */
     </style>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        var form = document.querySelector(".login_form");
+
+        // Attach a submit event listener to the form
+        form.addEventListener("submit", function (e) {
+            e.preventDefault(); // Prevent the default form submission
+
+            // Collect form data
+            var formData = new FormData(this);
+
+            // Send the data to the server using AJAX
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "../controller/profileSetupAction", true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    // Handle the server's response (if needed)
+                    console.log(xhr.responseText);
+
+                    // Check for a successful response (modify this condition as needed)
+                    if (xhr.responseText === "Data saved successfully") {
+                        // Reset the form after successful submission
+                        alert('hari');
+                        form.reset();
+                    }
+                }
+            };
+            xhr.send(formData);
+        });
+    });
+</script>
 </head>
 <body>
     <div class="logo_container">
@@ -115,7 +146,7 @@ require_once __DIR__."/../config/SessionConfig.php";
         <div class="headline_prof_set">
             <h1>Setup your profile</h1>
         </div>
-        <form action="../controller/profileSetupAction.php" method="POST">
+        <form action="../controller/profileSetupAction" method="POST" class="login_form">
             <div class="cont_prof_set">
                 <h3 class="prof_set_head">Add Profile Picture</h3>
                 <div class="profile_pic_upl_container">
@@ -143,7 +174,7 @@ require_once __DIR__."/../config/SessionConfig.php";
                 <button class="set_form_act_btn" type="submit">Upload Profile</button>
                 <input class="set_form_act_btn" type="submit" value="Skip for Now">
             </div>
-        </form>        
+        </form>
     </div> 
 </body>
 </html>
