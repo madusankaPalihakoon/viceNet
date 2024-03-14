86f63abedef1129a7f10e21a275d3106cea0711e
Madusanka Palihakoon <pmsmpalihakoon@gmail.com>
2024-03-14 09:16:09 +0530

    laravel backend first step

--- assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-const homeLink = document.getElementById('homeLink');
-const profileLink = document.getElementById('profileLink');
-const friendsLink = document.getElementById('friendsLink');
-const settingLink = document.getElementById('settingLink');
-
-homeLink.setAttribute('href','home');
-profileLink.setAttribute('href','profile');
-friendsLink.setAttribute('href','friends');
-settingLink.setAttribute('href','setting');
